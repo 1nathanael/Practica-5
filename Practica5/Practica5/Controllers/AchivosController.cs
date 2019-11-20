1b1e1aa379d5ab942cc5bf4725e6e1fd5f60d0c3
raynier lara <raynier98@hotmail.es>
2019-11-19 23:57:17 -0400

    codigos para introducir las filas de la tabla de exel y, dependiendo de los campos que tenga, incluyendo arreglos

--- Practica5/Practica5/Controllers/AchivosController.cs
+++ Practica5/Practica5/Controllers/AchivosController.cs
@@ -1,6 +1,7 @@
 ﻿using Practica5.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -24,7 +25,31 @@ namespace Practica5.Controllers
             if(postedFile != null)
             {
                 string ruta = Server.MapPath("~/Archivos/");
+                if (!Directory.Exists(ruta))
+                {
+                    Directory.CreateDirectory(ruta);
+                }
+
+                archivo = ruta + Path.GetFileName(postedFile.FileName);
+                string extension = Path.GetExtension(postedFile.FileName);
+                postedFile.SaveAs(archivo);
+
+                string Dexel = System.IO.File.ReadAllText(archivo);
+                foreach (string row in Dexel.Split('\n'))
+                {
+                    if (!string.IsNullOrEmpty(row))
+                    {
+                        M.Add(new Modificador
+                        {
+                            ID = Convert.ToInt32(row.Split(',')[0]),
+                            Nombre = row.Split(',')[1],
+                            Descripcion = row.Split(',')[2]
+                        });
+                    }
+                }
             }
+
+            return View(M);
         }
     }
 }
