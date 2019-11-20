db7ce1a16354320842fcab028cae89404a1b7291
raynier lara <raynier98@hotmail.es>
2019-11-19 23:11:08 -0400

    Creando C# " modificador" para que reciba los datos del archivo exel

---
+++ Practica5/Practica5/Controllers/AchivosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practica5.Controllers
+{
+    public class AchivosController : Controller
+    {
+        // GET: Achivos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Practica5/Practica5/Models/Modificador.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practica5.Models
+{
+    public class Modificador
+    {
+        public int ID { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+    }
+}
