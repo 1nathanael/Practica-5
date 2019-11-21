a8b5f1094effd07aa226063199a96ccbe621c78e
nathanael <nathanaelcedeno@hotmail.com>
2019-11-21 16:04:27 -0400

    Controlador Contacto creado apartir del modelo!

---
+++ Practica5/Practica5/Controllers/ContactController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Practica5.Models;
+
+namespace Practica5.Controllers
+{
+    public class ContactController : Controller
+    {
+        private AgendaElectronicaEntities1 db = new AgendaElectronicaEntities1();
+
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View(db.Contactos.ToList());
+        }
+
+        // GET: Contact/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contactos contactos = db.Contactos.Find(id);
+            if (contactos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactos);
+        }
+
+        // GET: Contact/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Contact/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Nombre,Apellido,Celular,Email,Direccion")] Contactos contactos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Contactos.Add(contactos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(contactos);
+        }
+
+        // GET: Contact/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contactos contactos = db.Contactos.Find(id);
+            if (contactos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactos);
+        }
+
+        // POST: Contact/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Nombre,Apellido,Celular,Email,Direccion")] Contactos contactos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contactos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(contactos);
+        }
+
+        // GET: Contact/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contactos contactos = db.Contactos.Find(id);
+            if (contactos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactos);
+        }
+
+        // POST: Contact/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Contactos contactos = db.Contactos.Find(id);
+            db.Contactos.Remove(contactos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Practica5/Practica5/Models/Model1.Designer.cs
+++ Practica5/Practica5/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Raynier\Source\Repos\Practica-5\Practica5\Practica5\Models\Model1.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\User\Source\Repos\Practica-5\Practica5\Practica5\Models\Model1.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
